cc5a7c7c7e69816aba807c7b1c513cbc375cf6fe
Resma Adi Nugroho <resmaadi@students.undip.ac.id>
2024-01-10 09:45:20 +0700

    fix: validation login

--- routes/login.dart
+++ routes/login.dart
@@ -3,19 +3,28 @@ import 'package:rs_dart_backend/core/core_system.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   if (context.request.method == HttpMethod.post) {
-    final body = await context.request.json() as Map<String, dynamic>;
-    if (body['username'] == 'admin' && body['password'] == 'admin') {
-      return CoreResponse.ok(
-        body: {
-          'id': 1,
-          'username': 'admin',
-          'name': 'Administrator',
-        },
-      );
+    final bodyCheck = await context.request.body();
+    if (bodyCheck.isNotEmpty) {
+      final body = await context.request.json();
+      if (body['username'] == 'admin' && body['password'] == 'admin') {
+        return CoreResponse.ok(
+          body: {
+            'id': 1,
+            'username': 'admin',
+            'name': 'Administrator',
+          },
+        );
+      } else {
+        return CoreResponse.error(
+          body: {
+            'message': 'Username atau password salah',
+          },
+        );
+      }
     } else {
       return CoreResponse.error(
         body: {
-          'message': 'Username atau password salah',
+          'message': 'Data harus format JSON',
         },
       );
     }
